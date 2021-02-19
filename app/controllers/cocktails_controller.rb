dca8d04abe18e355e151ff5c41eb4b26eb38207e
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-18 21:11:37 -0300

    Mister Cocktail Exercise - Day 1

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,36 @@
+class CocktailsController < ApplicationController
+  before_action :find, only: %i[show]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  # before show, edit, update and destroy run this code!
+  def find
+    @cocktail = Cocktail.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+
+cocktail = Cocktail.create!(name: 'Blue Lagoon')
+Cocktail.create!(name: 'Caipirinha')
+Cocktail.create!(name: 'Margarita')
+
+Ingredient.destroy_all
+Ingredient.create!(name: 'ice')
+ingredient = Ingredient.create!(name: 'lemon')
+Ingredient.create!(name: 'mint leaves')
+
+Dose.create!(description: 'teste', cocktail: cocktail, ingredient: ingredient)
