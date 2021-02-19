d915108b8645fed2a7baae91634752553f902645
Joao Brufatto <joao.brufatto@gmail.com>
2021-02-19 18:01:58 -0300

    More fixes...

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,38 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Cocktail.destroy_all
 
-pngblue = URI.open('https://i.imgur.com/tC7dDXN.png')
-blue = Cocktail.new(name: 'Blue Lagoon', bootstrap_color: 'primary')
-blue.photo.attach(io: pngblue, filename: 'blue.png', content_type: 'image/png')
-blue.save
-
-pngcaip = URI.open('https://i.imgur.com/GqY6EzJ.png')
-caipirinha = Cocktail.new(name: 'Caipirinha', bootstrap_color: 'info')
-caipirinha.photo.attach(io: pngcaip, filename: 'caip.png', content_type: 'image/png')
-caipirinha.save
-
-pngmarg = URI.open('https://i.imgur.com/ZKJrWSv.png')
-margarita = Cocktail.new(name: 'Margarita', bootstrap_color: 'danger')
-margarita.photo.attach(io: pngmarg, filename: 'caip.png', content_type: 'image/png')
-margarita.save
-
 Ingredient.destroy_all
-ice = Ingredient.create!(name: 'ice')
-lemon = Ingredient.create!(name: 'lemon')
+Ingredient.create!(name: 'ice')
+Ingredient.create!(name: 'lemon')
 Ingredient.create!(name: 'mint leaves')
+Ingredient.create!(name: 'sugar')
+Ingredient.create!(name: 'vodka')
+Ingredient.create!(name: 'gim')
 
-Dose.create!(
-  description: '10',
-  cocktail_id: blue.id,
-  ingredient: ice
-)
+# pngblue = URI.open('https://i.imgur.com/tC7dDXN.png')
+# blue = Cocktail.new(name: 'Blue Lagoon', bootstrap_color: 'primary')
+# blue.photo.attach(io: pngblue, filename: 'blue.png', content_type: 'image/png')
+# blue.save
+#
+# pngcaip = URI.open('https://i.imgur.com/GqY6EzJ.png')
+# caipirinha = Cocktail.new(name: 'Caipirinha', bootstrap_color: 'info')
+# caipirinha.photo.attach(io: pngcaip, filename: 'caip.png', content_type: 'image/png')
+# caipirinha.save
+#
+# pngmarg = URI.open('https://i.imgur.com/ZKJrWSv.png')
+# margarita = Cocktail.new(name: 'Margarita', bootstrap_color: 'danger')
+# margarita.photo.attach(io: pngmarg, filename: 'marg.png', content_type: 'image/png')
+# margarita.save
+# =
 
-Dose.create!(
-  description: '5',
-  cocktail_id: caipirinha.id,
-  ingredient: lemon
-)
+# Dose.create!(
+#   description: '10',
+#   cocktail_id: blue.id,
+#   ingredient: ice
+# )
+#
+# Dose.create!(
+#   description: '5',
+#   cocktail_id: caipirinha.id,
+#   ingredient: lemon
+# )
